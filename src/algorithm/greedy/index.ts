2f37e894a9032ce8896cd154fa2d183008c368b9
dangminh214 <dang_minh.nguyen@stud.tu-darmstadt.de>
2026-01-19 23:12:29 +0100

    add readme

--- src/algorithm/greedy/index.ts
+++ src/algorithm/greedy/index.ts
@@ -21,23 +21,11 @@ export class Greedy<I extends IItem, S extends Solution> {
         this.placementStrategy = placementStrategy;
     }
 
-    /**
-     * Executes the greedy approach.
-     * @param problem The problem instance providing unplaced items.
-     * @param placement The specific selection strategy to use.
-     * @param onStep Optional callback for the GUI to visualize progress
-     */
     solve(items: I[]): S {
         const start = performance.now();
 
-        // 1. Get the raw data from the problem
-        // const items = this.selection.getUnplacedItems();
-
         // 2. Order items based on the selection strategy (e.g., Area, Longest Side)
         const orderedItems = this.selectionStrategy.orderItems(items);
-
-        // 3. Get the next item based on strategy
-        // const item = this.selectionStrategy.getNextItem();
 
         // 3. Start with an empty solution state
         // because the items are already ordered, if they are fine, add them imidiatly
